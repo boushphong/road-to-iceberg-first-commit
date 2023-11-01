bfe78c1e7754dbc2cbe51446652ec15666dfda35
Phong Bui <boushphong@gmail.com>
2023-11-01 15:47:08 +0700

    Refactor SparkConf

--- src/main/scala/IcebergSparkJob.scala
+++ src/main/scala/IcebergSparkJob.scala
@@ -1,22 +1,25 @@
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object IcebergSparkJob extends App {
-  private val spark = SparkSession
-    .builder()
-    .appName("Iceberg Spark Job")
-    .master("local")
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
-    .config("spark.sql.catalog.local.catalog-impl", "org.apache.iceberg.jdbc.JdbcCatalog")
-    .config("spark.sql.catalog.local.uri", "jdbc:postgresql://localhost:5432/postgres")
-    .config("spark.sql.catalog.local.jdbc.user", "postgres")
-    .config("spark.sql.catalog.local.jdbc.password", "postgres")
-    .config("spark.sql.catalog.my_catalog.jdbc.verifyServerCertificate", value = true)
-    .config("spark.sql.catalog.my_catalog.jdbc.useSSL", value = true)
-    .config("spark.sql.catalog.local.warehouse", "./spark-warehouse")
-    .config("spark.sql.defaultCatalog", "local")
-    .config("spark.sql.catalogImplementation", "in-memory")
-    .getOrCreate()
+  // Create a SparkConf object and set all the configurations
+  private val sparkConf = new SparkConf()
+    .setAppName("Iceberg Spark Job")
+    .setMaster("local")
+    .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+    .set("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
+    .set("spark.sql.catalog.local.catalog-impl", "org.apache.iceberg.jdbc.JdbcCatalog")
+    .set("spark.sql.catalog.local.uri", "jdbc:postgresql://localhost:5432/postgres")
+    .set("spark.sql.catalog.local.jdbc.user", "postgres")
+    .set("spark.sql.catalog.local.jdbc.password", "postgres")
+    .set("spark.sql.catalog.my_catalog.jdbc.verifyServerCertificate", "true")
+    .set("spark.sql.catalog.my_catalog.jdbc.useSSL", "true")
+    .set("spark.sql.catalog.local.warehouse", "./spark-warehouse")
+    .set("spark.sql.defaultCatalog", "local")
+    .set("spark.sql.catalogImplementation", "in-memory")
+
+  // Create a SparkSession using the SparkConf
+  private val spark = SparkSession.builder.config(sparkConf).getOrCreate()
 
   spark.sql("DROP TABLE IF EXISTS warehouse.dataset")
 
